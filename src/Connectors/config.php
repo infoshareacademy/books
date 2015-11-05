a19800dc7c22053576222344748e847e2e28d157
Paweł Zawodny <pawel.zawodny@gmai.com>
2015-11-05 16:59:42 +0100

    Developer sandbox config

--- src/Connectors/config.php
+++ src/Connectors/config.php
@@ -1,5 +1,5 @@
 <?php
 define('COUNTRY_CODE', 1);
 define('WEBAPI_USER_LOGIN', 'zawodny_j');
-define('WEBAPI_USER_ENCODED_PASSWORD', base64_encode(hash('sha256', 'jurek123', true)));
-define('WEBAPI_KEY', '69edff3c');
+define('WEBAPI_USER_ENCODED_PASSWORD', 'ccb453ac45c4b250');
+define('WEBAPI_KEY', 'sccb453a');
