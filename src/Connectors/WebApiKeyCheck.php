06f5dc4edfc60ba9a6d878a2562379662f751304
Paweł Zawodny <pawel.zawodny@gmail.com>
2015-11-09 17:04:52 +0100

    Quick connection fix in index php.

--- src/Connectors/WebApiKeyCheck.php
+++ src/Connectors/WebApiKeyCheck.php
@@ -8,21 +8,24 @@ class WebApiKeyCheck
     {
         $config = array(
 
-            'login' => 'zawodny_j',
-            'password' => 'jurek123',
-            'apiKey' => '69edff3c'
+            'userLogin' => 'zawodny_j',
+            'userPassword' => 'jurek123',
+            'WebapiKey' => '69edff3c',
 
         );
 
-        $country = WebApiSoapClient::COUNTRY_PL;
+        $countryCode = WebApiSoapClient::COUNTRY_PL;
 
         try {
 
+            print_r('fsdfdsfsd');
             $client = new WebAPISoapClient();
             // pobieranie wersji WebAPI
-            $version = $client->doQuerySysStatus(WebAPISoapClient::QUERY_ALLEGROWEBAPI, $country, $config['apiKey']);
+            $version = $client->doQuerySysStatus(WebAPISoapClient::QUERY_ALLEGROWEBAPI, $countryCode, $config['WebapiKey']);
             // właściwe logowanie do serwisu
-            $session = $client->doLogin($config['login'], $config['password'], $country, $config['apiKey'], $version['ver-key']);
+            $session = $client->doLogin($config['userlogin'], $config['userpassword'], $countryCode, $config['WebapiKey'], $version['ver-key']);
+            $list = $client->doGetItemList();
+            var_dump($list);
         } catch
         (SoapFault $soapFault) {
             /* obsługa wyjątku */
--- web/index.php
+++ web/index.php
@@ -6,10 +6,44 @@ use Books\Connectors\AllegroApiConnector as Connector;
 $app = new Silex\Application();
 $app['debug'] = true;
 
-return Connector::getWebApiKey();
-$app->get('/getkey', function () use ($app) {
+$app->get('/', function () use ($app) {
 
-    return $app->json(Connector::getWebApiKey());
+    $webapiKey = '69edff3c';
+    $userLogin ='zawodny_j';
+    $userPassword = 'jurek123';
+    $session = null;
+
+    $result = [];
+
+    $soapClient = new \SoapClient('https://webapi.allegro.pl/service.php?wsdl');
+
+    $result['sysStatus'] = $soapClient->doQueryAllSysStatus([
+        'countryId' => 1,
+        'webapiKey' => $webapiKey
+    ]);
+
+    $result['itemList'] = $soapClient->doGetItemsList([
+        'countryId' => 1,
+        'webapiKey' => $webapiKey
+    ]);
+
+    $localVersion = (array) $soapClient->doQuerySysStatus([
+        'sysvar' => 1,
+        'countryId' => 1,
+        'webapiKey' => $webapiKey
+    ]);
+
+    $session = $soapClient->doLogin([
+        'userLogin' => $userLogin,
+        'userPassword' => $userPassword,
+        'countryCode' => 1,
+        'webapiKey' => $webapiKey,
+        'localVersion' => $localVersion['verKey']
+    ]);
+
+    $result['session'] = $session;
+
+    return $app->json($result);
 });
 
 $app->run();
