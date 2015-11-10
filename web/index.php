72a602772dd8d7c9bb58203a16131441ca14887b
Paweł Zawodny <pawel.zawodny@gmail.com>
2015-11-10 14:50:14 +0100

    Cleaned and reformatted version of previous commit.

--- web/index.php
+++ web/index.php
@@ -1,6 +1,6 @@
 <?php
 
-require_once __DIR__.'/../vendor/autoload.php';
+require_once __DIR__ . '/../vendor/autoload.php';
 
 $app = new Silex\Application();
 $app['debug'] = true;
@@ -8,43 +8,21 @@ $app['debug'] = true;
 $app->get('/', function () use ($app) {
 
     $webapiKey = '69edff3c';
-//    $userLogin ='zawodny_j';
-//    $userPassword = 'jurek123';
-//    $session = null;
 
     $result = [];
 
     $soapClient = new \SoapClient('https://webapi.allegro.pl/service.php?wsdl');
 
-//    $result['sysStatus'] = $soapClient->doQueryAllSysStatus([
-//        'countryId' => 1,
-//        'webapiKey' => $webapiKey
-//    ]);
 
     $result['itemList'] = $soapClient->doGetItemsList([
         'filterOptions' => [[
-          'filterId' => 'category',
+            'filterId' => 'category',
             'filterValueId' => [5893]
         ]],
         'countryId' => 1,
         'webapiKey' => $webapiKey
     ]);
 
-//    $localVersion = (array) $soapClient->doQuerySysStatus([
-//        'sysvar' => 1,
-//        'countryId' => 1,
-//        'webapiKey' => $webapiKey
-//    ]);
-//
-//    $session = $soapClient->doLogin([
-//        'userLogin' => $userLogin,
-//        'userPassword' => $userPassword,
-//        'countryCode' => 1,
-//        'webapiKey' => $webapiKey,
-//        'localVersion' => $localVersion['verKey']
-//    ]);
-
-//    $result['session'] = $session;
 
     return $app->json($result['itemList']->itemsList);
 });
