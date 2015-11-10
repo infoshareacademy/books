605982cb635b853da65be2e08df64b053349cea1
Paweł Zawodny <pawel.zawodny@gmail.com>
2015-11-10 14:46:55 +0100

    Final and simplest version of download ebook list from allegro api. Commented examples of using other methods, that may be useful in the future.

--- web/index.php
+++ web/index.php
@@ -1,7 +1,6 @@
 <?php
 
 require_once __DIR__.'/../vendor/autoload.php';
-use Books\Connectors\AllegroApiConnector as Connector;
 
 $app = new Silex\Application();
 $app['debug'] = true;
@@ -9,41 +8,45 @@ $app['debug'] = true;
 $app->get('/', function () use ($app) {
 
     $webapiKey = '69edff3c';
-    $userLogin ='zawodny_j';
-    $userPassword = 'jurek123';
-    $session = null;
+//    $userLogin ='zawodny_j';
+//    $userPassword = 'jurek123';
+//    $session = null;
 
     $result = [];
 
     $soapClient = new \SoapClient('https://webapi.allegro.pl/service.php?wsdl');
 
-    $result['sysStatus'] = $soapClient->doQueryAllSysStatus([
-        'countryId' => 1,
-        'webapiKey' => $webapiKey
-    ]);
+//    $result['sysStatus'] = $soapClient->doQueryAllSysStatus([
+//        'countryId' => 1,
+//        'webapiKey' => $webapiKey
+//    ]);
 
     $result['itemList'] = $soapClient->doGetItemsList([
+        'filterOptions' => [[
+          'filterId' => 'category',
+            'filterValueId' => [5893]
+        ]],
         'countryId' => 1,
         'webapiKey' => $webapiKey
     ]);
 
-    $localVersion = (array) $soapClient->doQuerySysStatus([
-        'sysvar' => 1,
-        'countryId' => 1,
-        'webapiKey' => $webapiKey
-    ]);
+//    $localVersion = (array) $soapClient->doQuerySysStatus([
+//        'sysvar' => 1,
+//        'countryId' => 1,
+//        'webapiKey' => $webapiKey
+//    ]);
+//
+//    $session = $soapClient->doLogin([
+//        'userLogin' => $userLogin,
+//        'userPassword' => $userPassword,
+//        'countryCode' => 1,
+//        'webapiKey' => $webapiKey,
+//        'localVersion' => $localVersion['verKey']
+//    ]);
 
-    $session = $soapClient->doLogin([
-        'userLogin' => $userLogin,
-        'userPassword' => $userPassword,
-        'countryCode' => 1,
-        'webapiKey' => $webapiKey,
-        'localVersion' => $localVersion['verKey']
-    ]);
+//    $result['session'] = $session;
 
-    $result['session'] = $session;
-
-    return $app->json($result);
+    return $app->json($result['itemList']->itemsList);
 });
 
 $app->run();
